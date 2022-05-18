b9bba2f664ad2a1a38b15ca575743daae5e0ca5e
unknown <levuanh11>
2022-05-18 19:10:16 +0700

    PLPLPLPL

---
+++ bai2.js
@@ -0,0 +1,15 @@
+function footToMeter(foot) {
+    let meter = 0.305 * foot;
+    return meter;
+}
+
+function meterToFoot(meter) {
+    let foot = 3.279 * meter;
+    return foot;
+}
+
+let foot = prompt("Nhập foot: ");
+document.write(foot + " foot " + "= " + footToMeter(foot) + " meter");
+document.write("<br>")
+let meter = prompt("Nhập meter: ");
+document.write(meter + " meter " + "= " + meterToFoot(meter) + " foot");
